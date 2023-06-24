d972e81006efbfd9dd832ef2399ea9009fb0afbf
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-06-24 08:25:01 +0300

    completed project

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymysql
+pymysql.install_as_MySQLdb()
+from models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:p@55word@localhost/flask_lab'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
